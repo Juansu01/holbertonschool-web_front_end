53fc00c78981e5a8108d9c54290dbaaa3583e614
Juansu01 <thejuank0@hotmail.com>
2022-05-20 11:58:30 -0500

    Add task 11

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,30 @@
+const { performance } = require('perf_hooks');
+
+
+function countPrimeNumbers() {
+
+    function isPrime(num) {
+        for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+            if(num % i === 0) return false;
+        return num > 1;
+    }
+
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+
+    return count
+}
+
+const timeFirst = performance.now();
+for (let i = 1; i <= 100; i++) {
+    setTimeout(() => {
+        countPrimeNumbers();
+    }, 0);
+}
+const timeSecond = performance.now();
+
+console.log(`Execution time of printing countPrimeNumbers was ${timeSecond - timeFirst} milliseconds.`)
