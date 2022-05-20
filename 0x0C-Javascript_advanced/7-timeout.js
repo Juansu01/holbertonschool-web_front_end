268a2d815821c4ecae9c4d3279404a737a94b9d7
Juansu01 <thejuank0@hotmail.com>
2022-05-20 11:55:50 -0500

    Fix timeout

--- 0x0C-Javascript_advanced/7-timeout.js
+++ 0x0C-Javascript_advanced/7-timeout.js
@@ -3,7 +3,7 @@ console.log('Start of the execution queue');
 
 setTimeout(() => {
     console.log('Final code block to be executed');
-}, 1)
+}, 0)
 
 for (let i = 1; i <= 100; i++) {
     console.log(i);
