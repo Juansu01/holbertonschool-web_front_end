1292151ddbdef881b21c5f6fcbc694e15b8e69ff
Juansu01 <thejuank0@hotmail.com>
2022-05-20 14:43:11 -0500

    Add task 14

---
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,25 @@
+
+function createElement(data) {
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = data;
+    document.body.appendChild(paragraph);
+}
+
+
+function queryWikipedia(callback) {
+    const req = new XMLHttpRequest();
+    req.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*');
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status === 200) {
+                callback(JSON.parse(req.responseText));
+            } else {
+                const error = new Error('Error');
+                return callback(error, null);
+            }
+        }
+    }
+    req.send();
+}
+
+queryWikipedia(createElement);
